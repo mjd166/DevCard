36fa12c59f46bb8c2425a1904fa610aa6105e09e
mjd166 <mghafari41@yahoo.com>
2022-03-05 12:41:11 +0330

    add partial view for special project secion in index

---
+++ DevCard_MVC/Models/Article.cs
@@ -0,0 +1,13 @@
+﻿namespace DevCard_MVC.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
